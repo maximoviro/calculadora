2381f4aa14ddd183490d46e2d3c9b82a73e5a15f
Your Name <yosoymaximito@icloud.com>
2025-10-22 23:23:01 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const pantalla = document.querySelector('.pantalla');
+const botones = document.querySelectorAll('.btn');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+
+
+ const botonApretado = boton.textContent;
+
+if(boton.id === 'c') {textContent = '0'
+  return;
+}
+
+if (boton id === 'borrar') {
+if (pantalla.textContent.length === 1 
+) 
+}
+
+
+    }) 
+})
